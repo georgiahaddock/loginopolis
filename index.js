8ee25cde2d5d170c5a44a14e26d269160cbbda96
georgiahaddock <116069149+georgiahaddock@users.noreply.github.com>
2023-01-18 11:33:57 +0000

    login method added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+const saltCount = 9
 
 app.get('/', async (req, res, next) => {
   try {
@@ -21,7 +22,7 @@ app.post('/register', async (req, res, next) => {
   try{
     const username = req.body.username;
     const password = req.body.password;
-    const hash     = await bcrypt.hash(password, 9);
+    const hash     = await bcrypt.hash(password, saltCount);
     const [user] = await User.create({username, password: hash});
     const message = `successfully created user ${username}`;
     res.send(message);
@@ -34,6 +35,26 @@ app.post('/register', async (req, res, next) => {
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post('/login', async (req, res, next)=>{
+  try{
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: {username} });
+    if(!user){
+      res.status(401).send('user not found');
+      return;
+    }
+    verified = await bcrypt.compare(password, user.password);
+    if(!verified){
+      res.send('password incorrect');
+      return;
+    }
+    res.status(401).send(`successfully logged in user ${username}`);
+
+  }catch(error){
+    console.error(error);
+    next(error);
+  }
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
